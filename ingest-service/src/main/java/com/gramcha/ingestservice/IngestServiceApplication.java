73a7d179e1c0d7732c26aa7a4cb85566b03c22d2
Ganeshkumar Ramachandran <haiganeshkumar@gmail.com>
2018-07-08 01:18:42 +0530

    component scan for bean search from test code

--- ingest-service/src/main/java/com/gramcha/ingestservice/IngestServiceApplication.java
+++ ingest-service/src/main/java/com/gramcha/ingestservice/IngestServiceApplication.java
@@ -2,8 +2,10 @@ package com.gramcha.ingestservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = { "com.gramcha.*"})
 public class IngestServiceApplication {
 
 	public static void main(String[] args) {
