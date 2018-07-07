82a3842391fb8357f3b76f0b9292d3baa0aa47fb
Ganeshkumar Ramachandran <haiganeshkumar@gmail.com>
2018-07-07 15:18:03 +0530

    ingest-service: springboot boilerplate code

---
+++ ingest-service/src/main/java/com/gramcha/ingestservice/IngestServiceApplication.java
@@ -0,0 +1,12 @@
+package com.gramcha.ingestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngestServiceApplication.class, args);
+	}
+}
---
+++ ingest-service/src/test/java/com/gramcha/ingestservice/IngestServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gramcha.ingestservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IngestServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
