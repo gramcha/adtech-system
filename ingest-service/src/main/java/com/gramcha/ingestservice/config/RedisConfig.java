768cfc6da172c97df28b4d9f599b18fb94bfe95d
Ganeshkumar Ramachandran <haiganeshkumar@gmail.com>
2018-07-07 18:45:25 +0530

    redis connection creator from config

---
+++ ingest-service/src/main/java/com/gramcha/ingestservice/config/RedisConfig.java
@@ -0,0 +1,31 @@
+/**
+ * @author gramcha
+ * 07-Jul-2018 4:02:06 PM
+ * 
+ */
+package com.gramcha.ingestservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
+import org.springframework.data.redis.serializer.GenericToStringSerializer;
+
+@Configuration
+public class RedisConfig {
+	@Bean
+    JedisConnectionFactory jedisConnectionFactory() {
+        return new JedisConnectionFactory();
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        final RedisTemplate<String, Object> template = new RedisTemplate<String, Object>();
+        template.setConnectionFactory(jedisConnectionFactory());
+        template.setValueSerializer(new GenericToStringSerializer<Object>(Object.class));
+        return template;
+    }    
+}
