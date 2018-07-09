aae7f55498b0f986062279cc91f2d64a8b77ab06
Ganeshkumar Ramachandran <haiganeshkumar@gmail.com>
2018-07-10 00:33:11 +0530

    removing consumer part from ingest service

--- ingest-service/src/main/java/com/gramcha/ingestservice/service/KafkaDataPipeLineService.java
+++ ingest-service/src/main/java/com/gramcha/ingestservice/service/KafkaDataPipeLineService.java
@@ -5,21 +5,16 @@
  */
 package com.gramcha.ingestservice.service;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Service;
 
-import com.gramcha.ingestservice.entities.ClickTracker;
-import com.gramcha.ingestservice.entities.DeliveryTracker;
-import com.gramcha.ingestservice.entities.InstallTracker;
+import com.gramcha.entities.ClickTracker;
+import com.gramcha.entities.DeliveryTracker;
+import com.gramcha.entities.InstallTracker;
 
 @Service
 public class KafkaDataPipeLineService implements DataPipeLineService {
-	private final Logger LOG = LoggerFactory.getLogger(getClass());
 	private final static String DELIVERY_TOPIC = "delivery";
 	private final static String CLICK_TOPIC = "click";
 	private final static String INSTALL_TOPIC = "install";
@@ -45,29 +40,5 @@ public class KafkaDataPipeLineService implements DataPipeLineService {
 	@Override
 	public void send(InstallTracker tracker) {
 		installProducer.send(INSTALL_TOPIC, tracker.getInstallId(), tracker);
-	}
-
-	// Below listeners are just to validate the kafka producer will be removed once
-	// store-service(kafka consumer) implemented.
-	//TODO: remove listeners from this service
-	@KafkaListener(topics = DELIVERY_TOPIC)
-	void deliveryConsumer(DeliveryTracker deliveryTrackerEntity, Acknowledgment acknowledgment) {
-		LOG.info("KAFKA consumer ....");
-		LOG.info("Revceived delivery tracker " + deliveryTrackerEntity.getDeliveryId());
-		acknowledgment.acknowledge();
-	}
-
-	@KafkaListener(topics = CLICK_TOPIC)
-	void deliveryConsumer(ClickTracker clickTrackerEntity, Acknowledgment acknowledgment) {
-		LOG.info("KAFKA consumer ....");
-		LOG.info("Revceived click tracker " + clickTrackerEntity.getClickId());
-		acknowledgment.acknowledge();
-	}
-
-	@KafkaListener(topics = INSTALL_TOPIC)
-	void deliveryConsumer(InstallTracker installTrackerEntity, Acknowledgment acknowledgment) {
-		LOG.info("KAFKA consumer ....");
-		LOG.info("Revceived install tracker " + installTrackerEntity.getInstallId());
-		acknowledgment.acknowledge();
 	}
 }
