e733ef75317affff080f7e744311be6795bee7b3
Ganeshkumar Ramachandran <haiganeshkumar@gmail.com>
2018-07-11 16:44:07 +0530

    ss: cleanup

--- store-service/src/main/java/com/gramcha/storeservice/StoreServiceApplication.java
+++ store-service/src/main/java/com/gramcha/storeservice/StoreServiceApplication.java
@@ -1,17 +1,8 @@
 package com.gramcha.storeservice;
 
-import java.util.Date;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.gramcha.entities.DeliveryTracker;
-import com.gramcha.storeservice.service.StoreService;
 
 @SpringBootApplication
 @ComponentScan(basePackages = { "com.gramcha.*"})
