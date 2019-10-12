1751508865308d52df26f77e42a51bb1d8f13ed8
gramcha <gramcha@pando.in>
2019-10-13 01:05:17 +0530

    converting the controller function into async

--- ingest-service/src/main/java/com/gramcha/ingestservice/IngestServiceApplication.java
+++ ingest-service/src/main/java/com/gramcha/ingestservice/IngestServiceApplication.java
@@ -2,13 +2,28 @@ package com.gramcha.ingestservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @SpringBootApplication
-@ComponentScan(basePackages = { "com.gramcha.*"})
+@ComponentScan(basePackages = {"com.gramcha.*"})
+@EnableAsync
 public class IngestServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(IngestServiceApplication.class, args);
-	}
+    @Bean("threadPoolTaskExecutor")
+    public TaskExecutor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(20);
+        executor.setMaxPoolSize(1000);
+        executor.setWaitForTasksToCompleteOnShutdown(true);
+        executor.setThreadNamePrefix("Async-");
+        return executor;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(IngestServiceApplication.class, args);
+    }
 }
--- ingest-service/src/main/java/com/gramcha/ingestservice/controller/AdTrackerController.java
+++ ingest-service/src/main/java/com/gramcha/ingestservice/controller/AdTrackerController.java
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,6 +20,8 @@ import com.gramcha.ingestservice.models.AdClickTrackerRequest;
 import com.gramcha.ingestservice.models.AdDeliveryTrackerRequest;
 import com.gramcha.ingestservice.models.AdInstallTrackerRequest;
 import com.gramcha.ingestservice.service.TrackerIngestService;
+
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("/ads")
@@ -33,28 +36,31 @@ public class AdTrackerController {
 		return "ping pong";
 	}
 
+	@Async("threadPoolTaskExecutor")
 	@RequestMapping(value = "/delivery", method = RequestMethod.POST)
-	public ResponseEntity<?> deliveryTracker(@RequestBody AdDeliveryTrackerRequest deliveryPayload) {
+	public CompletableFuture<ResponseEntity<?>> deliveryTracker(@RequestBody AdDeliveryTrackerRequest deliveryPayload) {
 		LOG.info("delivery request received");
 		ingestService.ingestDeliveryTracker(deliveryPayload);
-		return new ResponseEntity<HttpStatus>(HttpStatus.OK);
+		return CompletableFuture.completedFuture(new ResponseEntity<HttpStatus>(HttpStatus.OK));
 	}
 
+	@Async("threadPoolTaskExecutor")
 	@RequestMapping(value = "/click", method = RequestMethod.POST)
-	public ResponseEntity<?> clickTracker(@RequestBody AdClickTrackerRequest clickPayload) {
+	public CompletableFuture<ResponseEntity<?>> clickTracker(@RequestBody AdClickTrackerRequest clickPayload) {
 		LOG.info("click request received");
 		if (ingestService.ingestClickTracker(clickPayload))
-			return new ResponseEntity<HttpStatus>(HttpStatus.OK);
+			return CompletableFuture.completedFuture(new ResponseEntity<HttpStatus>(HttpStatus.OK));
 		else
-			return new ResponseEntity<HttpStatus>(HttpStatus.NO_CONTENT);
+			return CompletableFuture.completedFuture(new ResponseEntity<HttpStatus>(HttpStatus.NO_CONTENT));
 	}
 
+	@Async("threadPoolTaskExecutor")
 	@RequestMapping(value = "/install", method = RequestMethod.POST)
-	public ResponseEntity<?> installTracker(@RequestBody AdInstallTrackerRequest installPayload) {
+	public CompletableFuture<ResponseEntity<?>> installTracker(@RequestBody AdInstallTrackerRequest installPayload) {
 		LOG.info("install request received");
 		if (ingestService.ingestInstallTracker(installPayload))
-			return new ResponseEntity<HttpStatus>(HttpStatus.OK);
+			return CompletableFuture.completedFuture(new ResponseEntity<HttpStatus>(HttpStatus.OK));
 		else
-			return new ResponseEntity<HttpStatus>(HttpStatus.NO_CONTENT);
+			return CompletableFuture.completedFuture(new ResponseEntity<HttpStatus>(HttpStatus.NO_CONTENT));
 	}
 }
